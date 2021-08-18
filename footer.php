aa54769a735ac1796a6e967fccd50d838ae85517
Kurtis Holsapple <kurtis@openfunctioncomputers.com>
2021-08-18 13:35:08 -0700

    updates and set up basic wp theme scaffold

---
+++ footer.php
@@ -0,0 +1,8 @@
+<?php
+
+wp_footer();
+
+echo site()->view("footer", [
+    "powered-by" => "ofco-better-wordpress",
+    "powered-by-url" => "https://github.com/open-function-computers-llc/better-wordpress",
+]);
---
+++ tpl/footer.tpl
@@ -0,0 +1,3 @@
+<footer class="site-footer">
+    <p>Powered by <a href="{{ powered-by-url }}" target="_blank">{{ powered-by }}</a></p>
+</footer>
