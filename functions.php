0a39f7bc4e6b71bfee779705e99648424de76fb5
Kurtis Holsapple <kurtis@openfunctioncomputers.com>
2025-05-07 21:08:00 -0700

    latest changes

--- functions.php
+++ functions.php
@@ -5,12 +5,22 @@ require __DIR__ . '/vendor/autoload.php';
 
 use ofc\Site;
 
-# Declare a new Site object
-$site = new Site();
-
-# Allow the Site object to be accessed in other files
-function site()
+/**
+ * Allow the Site object to be utilized anywhere
+ *
+ * @return Site
+ */
+function site(): Site
 {
-    global $site;
-    return $site;
+    return ofc\Site::getInstance();
 }
+
+// Hook into wordpress to set up the singleton, and now we're ready to go!
+add_action('after_setup_theme', 'site');
+
+// That's it! Literally never put any other code into this file, no matter what
+// you see on the internet. You can likely build out your theme using all the
+// tools provided, so if you feel like you need to put code here, instead open a
+// ticket here:
+//
+// https://github.com/open-function-computers-llc/rad-theme-engine-docs/issues
